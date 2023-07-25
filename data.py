c1cd7f6a60260ae5d31548f637159fe0930cf4bd
Mahdi Irani <iranimahdi79@gmail.com>
2023-07-25 19:54:40 +0330

    the script named data.py is complete and have 3 parameter and export in .csv format

--- data.py
+++ data.py
@@ -5,14 +5,43 @@ import requests
 import datetime
 import pandas as pd
 
-currency_pair = "eurusd"
+currency_pair = "btcusd"
 url = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
 
-params = {
-        "step":300,
-        "limit":1000,
-        }
+start = "2023-07-01"
+end = "2023-07-02"
+timeframe = 300
 
-data = requests.get(url, params=params)
+dates = pd.date_range(start, end, freq="1H")
+dates = [ int(x.value/10**9) for x in list(dates)]
+print(dates)
 
-print(data.json())
+master_data = []
+
+for first, last in zip(dates, dates[1:]):
+    print(first,last)
+    params = {
+            "step":timeframe,
+            "limit":1000,
+            "start":first,
+            "end":last,
+            }
+
+    data = requests.get(url, params=params)
+
+    data = data.json()["data"]["ohlc"]
+
+    master_data += data 
+
+df = pd.DataFrame(master_data)
+df = df.drop_duplicates()
+
+df["timestamp"] = df["timestamp"].astype(int)
+df = df.sort_values(by="timestamp")
+
+df = df[ df["timestamp"] >= dates[0] ]
+df = df[ df["timestamp"] < dates[-1] ]
+
+print(df)
+
+df.to_csv(f"{currency_pair}-{timeframe}|||{start}-{end}.csv", index=False)
